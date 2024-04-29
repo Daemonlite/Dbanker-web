99916833552520b6bdeba10e657eec5fd6a89fca
Daemonlite <khemikal@gmail.com>
2024-04-29 16:47:24 +0000

    components

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,30 @@
 import { createRouter,createWebHistory } from "vue-router";
 import Landing from '../components/Landing.vue'
+import About from '../components/About.vue'
+import Pricing from '../components/Pricing.vue'
+import Services from '../components/Services.vue'
+
 
 const routes = [
        {
         path:"/",
         name:"landing",
         component:Landing
+       },
+       {
+        path:"/about",
+        name:"about",
+        component:About
+       },
+       {
+        path:"/pricing",
+        name:"pricing",
+        component:Pricing
+       },
+       {
+        path:"/services",
+        name:"services",
+        component:Services
        }
 ]
 
